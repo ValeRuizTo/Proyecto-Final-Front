cb06ac65054e6a873bd2a94071eed54b6f6823c6
MariSalas23 <mari.masagu@gmail.com>
2024-05-03 18:41:49 -0500

    Create Routes

---
+++ my-react-app/src/pages/home/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="purple-container">
+                <img></img>
+            </div>
+            <div className="white-container">
+                <h1>socialGarden</h1>
+                <h2>¡Bienvenido!</h2>
+                <p>Por favor, ingresa tu sesión</p>
+                <button className="btn-login">Log In</button>
+                <button className="btn-signin">Log In</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
