13845f7a2efeb6e414ce9bc1639801dee34eab03
MariSalas23 <mari.masagu@gmail.com>
2024-05-03 21:55:56 -0500

    Home Responsive Design

--- my-react-app/src/pages/home/home.jsx
+++ my-react-app/src/pages/home/home.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import imgLogo from '../../assets/logo.jpg';
+import './home.css';
 
 const Home = () => {
     return (
         <div className="container">
             <div className="purple-container">
-                <img></img>
+                <div className="img-container">
+                    <img src={imgLogo} alt="Logo" />
+                </div>
             </div>
             <div className="white-container">
                 <h1>socialGarden</h1>
                 <h2>¡Bienvenido!</h2>
                 <p>Por favor, ingresa tu sesión</p>
                 <button className="btn-login">Log In</button>
-                <button className="btn-signin">Log In</button>
+                <button className="btn-signin">Sign In</button>
             </div>
         </div>
     )
