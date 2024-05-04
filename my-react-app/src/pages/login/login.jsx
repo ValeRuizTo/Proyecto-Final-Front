b7cee324ff97097b037c3ac4dea51257fae27baf
MariSalas23 <mari.masagu@gmail.com>
2024-05-04 14:44:36 -0500

    Responsive Design Login

--- my-react-app/src/pages/login/login.jsx
+++ my-react-app/src/pages/login/login.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import imgPeople from '../../assets/people.jpg';
+import './login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Log in</h1>
+        <div className="container-login">
+            <div className="white-container-login">
+                <div className="image-container-login">
+                    <img src={imgPeople} alt="Logo" />
+                </div>
+                <div className="text-container-login">
+                    <h1>Log In</h1>
+                    <form className="form-column">
+                        <label htmlFor="username">Usuario:</label>
+                        <input className="input-username" type="text" placeholder="Ingresa tu usuario" />
+                        <label htmlFor="user-password">Contraseña:</label>
+                        <input className="input-password" type="password" placeholder="Ingresa tu contraseña" />
+                    </form>
+                    <button className="btn-ingresar">Ingresar</button>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Login
+export default Login;
