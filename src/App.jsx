0b84274a5e2ede4ed0e71a710b5706bdd3dacc2b
MariSalas23 <mari.masagu@gmail.com>
2024-05-16 08:27:35 -0500

    Log Out Update

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path={"/login"} element={<Login />} />
-        <Route path="/tweets" element={<Navigate replace to={"/login"} />} />
+        <Route path="/tweets" element={<Navigate replace to={"/"} />} />
         </Routes>
       </Router>
     </AuthIsNotSignedIn>
