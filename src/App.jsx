a763f85490f79ce6044d7e8e6b50c719a4471fe4
MariSalas23 <mari.masagu@gmail.com>
2024-05-16 08:21:21 -0500

    Log In Log Out Prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import AuthProvider, { AuthIsNotSignedIn, AuthIsSignedIn} from "./contexts/AuthContext";
 import { Home, Login, Signin, Other, Success, Tweets} from './pages';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
+
+  <AuthProvider>
+    <AuthIsSignedIn>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/tweets" element={<Tweets />} />
+            <Route path="*" element={<Other />} />
+        </Routes>
+      </Router>
+    </AuthIsSignedIn>
+    <AuthIsNotSignedIn>
+      <Router>
+        <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/tweets" element={<Tweets />} />
-        <Route path="*" element={<Other />} />
-      </Routes>
-    </Router>
+        <Route path={"/login"} element={<Login />} />
+        <Route path="/tweets" element={<Navigate replace to={"/login"} />} />
+        </Routes>
+      </Router>
+    </AuthIsNotSignedIn>
+  </AuthProvider>
   );
 }
 
--- src/pages/tweets/tweets.jsx
+++ src/pages/tweets/tweets.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import {useContext} from 'react';
+import { AuthContext} from '../../contexts/AuthContext'; 
 import { Link } from 'react-router-dom';
 import './tweets.css';
 
 const Tweets = () => {
+    const { authStatus, signOut } = useContext(AuthContext);
+
+    const handleLogout = async () => {
+        localStorage.setItem("username", "");
+        signOut();
+        window.location.href = "/login";
+    };
+    
     return (
         <div className="container-success">
             <div className="white-container-success">    
                     <h1>¡TWEETS!</h1>
-                    <Link to="/" className="btn-home">Log Out</Link>
+                    <button  className="btn-home" onClick={handleLogout}>Logout</button>
             </div>
         </div>
     )
