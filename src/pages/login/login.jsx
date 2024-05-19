999c99d53614944a08379d342d39da8e1b0588cd
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-05-19 16:10:53 -0500

    prueba

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import imgPeople from '../../assets/people.jpg';
 import { AuthContext } from '../../contexts/AuthContext'; // Importar AuthContext
